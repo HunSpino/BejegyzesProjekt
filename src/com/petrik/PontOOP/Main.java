27709552a414251255cc3ca346b86b7942fda61b
HunSpino <tamasleventelaszlo99@gmail.com>
2021-09-29 16:29:37 +0200

    Egy lista létrehozása(amit előző commitnál elfelejtettem említeni) A oéda adatot kiírtam egy fügvénybe Az adatbekérés fügvényt elkezdtem

--- src/com/petrik/PontOOP/Main.java
+++ src/com/petrik/PontOOP/Main.java
@@ -2,15 +2,28 @@ package com.petrik.PontOOP;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     static List<Bejegyzes> bejegyzesek=new ArrayList<>();
+    static Scanner scnnr=new Scanner(System.in);
 
     public static void main(String[] args) {
+       bejegyzeles();
+       bekeregetes();
+
+    }
+
+
+    public static void bejegyzeles(){
         bejegyzesek.add(new Bejegyzes("MÁV","A 18:20-kor induló S60-as személyvonat a mai napon nem közlekedik"));
         bejegyzesek.add(new Bejegyzes("Barack Obama","After the elections,I will go down as a president"));
 
-        System.out.println(bejegyzesek.toString());
+    }
+    public static void bekeregetes(){
+        System.out.println("Írja meg hány bejegyzést szeretne beküldeni!");
+        int db= scnnr.nextInt();
+
     }
 }
