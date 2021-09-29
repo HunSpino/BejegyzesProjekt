36e14c14cca1377063d1a1424097a17bf17ba0ae
HunSpino <tamasleventelaszlo99@gmail.com>
2021-09-29 16:11:47 +0200

    Main-ben adtam meg pár példa adatot és kiíradtam

--- src/com/petrik/PontOOP/Main.java
+++ src/com/petrik/PontOOP/Main.java
@@ -1,8 +1,16 @@
 package com.petrik.PontOOP;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    static List<Bejegyzes> bejegyzesek=new ArrayList<>();
 
+    public static void main(String[] args) {
+        bejegyzesek.add(new Bejegyzes("MÁV","A 18:20-kor induló S60-as személyvonat a mai napon nem közlekedik"));
+        bejegyzesek.add(new Bejegyzes("Barack Obama","After the elections,I will go down as a president"));
+
+        System.out.println(bejegyzesek.toString());
     }
 }
