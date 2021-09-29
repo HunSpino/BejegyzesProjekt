773a6562e68722a0468490a98f3b189c10da40d3
HunSpino <tamasleventelaszlo99@gmail.com>
2021-09-29 15:10:10 +0200

    Bejegyzes java class létrehozva. Privát adatok létrehozása Getterek-setterek létrehozása

---
+++ src/com/petrik/PontOOP/Bejegyzes.java
@@ -0,0 +1,40 @@
+package com.petrik.PontOOP;
+
+import java.time.LocalDateTime;
+
+public class Bejegyzes {
+    private String szerzo;
+    private String tartalom;
+    private Integer likeok=0;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
+
+    public Bejegyzes(String szerzo, String tartalom) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+    }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+    }
+
+    public Integer getLikeok() {
+        return likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return szerkesztve;
+    }
+}
